a2bc0164e77947e134af666880070bde9cfd6c7a
marcpascalhofer <marc.hofer@students.ksba.ch>
2019-10-30 15:43:24 +0100

    Completed basic server w/ basic auth

--- routes/ats/ats.js
+++ routes/ats/ats.js
@@ -2,19 +2,61 @@ const mongoose = require("mongoose");
 const crypto = require("crypto");
 const basicAuth = require('basic-auth');
 
-
 const version = "1.0";
 const delay = 15;
 
-let sessions = {};
+let devices = {
+    client1: {
+        password: "passwd1",
+        sender: "sender1"
+    },
+    client2: {
+        password: "passwd2",
+        sender: "sender2"
+    },
+    sender1: {
+        password: "passwd3",
+        alarmOn: false,
+        activateAlarm: false,
+        deactivateAlarm: false
+    },
+    sender2: {
+        password: "passwd4",
+        alarmOn: false,
+        activateAlarm: false,
+        deactivateAlarm: false
+    }
+};
 let panic = false;
+
+let auth = function(req, res, next) {
+    let user = basicAuth(req);
+    if (!user || !user.name || !user.pass) {
+        res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
+        res.sendStatus(401);
+    } else if (!user.name in devices || user.pass === devices[user.name].password) {
+        res.sendStatus(403);
+    } else {
+        next();
+    }
+};
 
 module.exports = function (app) {
     console.log("ATS Server v" + version + " started");
-    app.post("/ats/client/heartbeat", function (req, res) { //main app
-
+    app.post("/ats/client/heartbeat", auth, function (req, res) { //main app
+        let user = basicAuth(req);
+        devices[devices[user.name].sender].activateAlarm = req.body.activateAlarm;
+        devices[devices[user.name].sender].deactivateAlarm = req.body.deactivateAlarm;
+        res.json({
+            alarmOn: devices[devices[user.name].sender].alarmOn,
+        })
     });
-    app.post("/ats/sender/heartbeat", function (req, res) { //main app
-
+    app.post("/ats/sender/heartbeat", auth, function (req, res) { //main app
+        let user = basicAuth(req);
+        devices[user.name].alarmOn = req.body.alarmOn;
+        res.json({
+            activateAlarm: devices[user.name].activateAlarm,
+            deactivateAlarm: devices[user.name].deactivateAlarm
+        })
     });
 };
